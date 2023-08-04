1c35b01d284d9cbc1b2fe2bd363a6ca707de6ea8
Gustavo Silva <guh_sz@outlook.com>
2023-08-04 17:24:40 -0300

    Create script.js

---
+++ aula17/script.js
@@ -0,0 +1,10 @@
+const nome = `Luiz Otávio`
+const sobrenome = `Miranda`
+const idade = `30`
+const peso = `84`
+const alturaEmM = 1.80;
+let imc;
+let anoNascimento=`2019`;
+imc=peso/(alturaEmM*alturaEmM);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} e tem ${alturaEmM} e nasceu em ${anoNascimento}. Seu IMC é de ${imc}`)
