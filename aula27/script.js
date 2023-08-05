ba261d469234262768b0bc50ca27f5400b847ea1
Gustavo Silva <guh_sz@outlook.com>
2023-08-05 12:43:09 -0300

    fim

---
+++ aula27/script.js
@@ -0,0 +1,10 @@
+let num1 = 9.44578;
+//let num2= Math.floor(num1);
+//let num2 = Math.round(num1);
+
+
+//console.log(Math.max(1,2,3,4,5,6,7,8,9));
+//console.log(Math.min(1,2,3,4,5,6,7,8,9));
+
+const aleatorio=Math.random()*(10-5)+5;
+console.log(aleatorio);
