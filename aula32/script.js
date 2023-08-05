3f85fc7592ba6dace2d9ad1234f10a7335cab0ce
Gustavo Silva <guh_sz@outlook.com>
2023-08-05 16:37:31 -0300

    Create script.js

---
+++ aula32/script.js
@@ -0,0 +1,20 @@
+//const pessoa = {
+ //   peso: "90kg",
+ //   idade: "11",
+  //  nome:"Lucas",
+ //   sobrenome: "Oliveira"
+
+//};
+
+//console.log(pessoa.peso)
+
+function criauser (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+    };
+}
+
+const pessoa1 = criauser('Gustavo','Silva',21);
+console.log(pessoa1.nome);
