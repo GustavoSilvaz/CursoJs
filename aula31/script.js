0996c15a8c23c1b5601801b3cbf899dfeb30e750
Gustavo Silva <guh_sz@outlook.com>
2023-08-05 13:59:50 -0300

    Create script.js

---
+++ aula31/script.js
@@ -0,0 +1,19 @@
+//function saudacao(nome){
+    
+   //return `Bom dia ${nome}!`;
+//}
+
+//const variavel = saudacao(`Luiz`);
+//console.log(variavel)
+
+//function soma(x,y) {
+    //return x + y;
+//}
+
+//console.log(soma(2,3))
+
+const raiz = function (n) {
+    return n**0.5;
+};
+
+console.log(raiz(9))
