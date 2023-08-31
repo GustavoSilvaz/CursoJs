ca40f954b17d618ca3f11ae505fbae6f998fa5a1
Gustavo Silva <guh_sz@outlook.com>
2023-08-31 08:57:06 -0300

    a

---
+++ aula26/assets/js/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('Evento previnido.');
+});
+
+function setResultado (msg) {
+    
+}
