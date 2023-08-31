5db5ccfb385840c955dcfde84d6ab09a8045dd7a
Gustavo Silva <guh_sz@outlook.com>
2023-08-31 09:12:19 -0300

    azaaaaaaa scrr

--- aula26/assets/js/script.js
+++ aula26/assets/js/script.js
@@ -3,8 +3,10 @@ const form = document.querySelector('#formulario');
 form.addEventListener('submit', function(event){
     event.preventDefault();
     console.log('Evento previnido.');
+    setResultado('Olá, mundo!');
 });
 
 function setResultado (msg) {
-    
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML=msg;
 }
