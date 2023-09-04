98c1483ef01b1b5e1f1044c40641fbd9842f9d47
Gustavo Silva <guh_sz@outlook.com>
2023-09-04 13:32:19 -0300

    metade

---
+++ modelo1/main.js
@@ -0,0 +1,18 @@
+const date = new Date();
+let diaSem = date.getDay();
+let diaSemText;
+let dia = date.getDate();
+let mes = date.getMonth();
+let mesText;
+let ano = date.getFullYear();
+let hora = date.getHours();
+let minuto = date.getMinutes();
+
+switch (diaSem) {
+        case 0:
+        diaSemText
+
+}
+
+
+
