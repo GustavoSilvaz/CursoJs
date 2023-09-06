6ee6e25b51986cde7c471ea1b0569165295e8d6c
Gustavo Silva <guh_sz@outlook.com>
2023-09-05 21:34:38 -0300

    Create main.js

---
+++ aula54/main.js
@@ -0,0 +1,9 @@
+const frutas = ['pera', 'uva,', 'maçã'];
+
+//for (let i = 0; i < frutas.length; i++) {
+//    console.log(frutas[i])
+//}
+
+for (let index in frutas) {
+    console.log(frutas[index]);
+}
