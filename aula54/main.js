ae2eee47f6446f7ccf91a761daa53b5649a0876c
Gustavo Silva <guh_sz@outlook.com>
2023-09-05 21:41:23 -0300

    Update main.js

--- aula54/main.js
+++ aula54/main.js
@@ -1,9 +1,24 @@
-const frutas = ['pera', 'uva,', 'maçã'];
+//onst frutas = ['pera', 'uva,', 'maçã'];
 
 //for (let i = 0; i < frutas.length; i++) {
 //    console.log(frutas[i])
 //}
 
-for (let index in frutas) {
-    console.log(frutas[index]);
-}
+//for (let index in frutas) {
+  //  console.log(frutas[index]);
+//}
+
+const pessoa  = {
+    nome: 'Luiz',
+    sobrenome:'Otávio',
+    idade: 50
+};
+
+
+
+//console.log(pessoa.nome);
+console.log(pessoa['nome']);
+
+//for (let chaves in pessoa){
+//    console.log(chaves)
+//}
